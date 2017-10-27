41715b8555979ebf60adc205ec17e6902559686a
Dowglas <dowglas_rodrigo@hotmail.com>
2017-10-27 11:44:02 -0200

    Selecionando na lista e exibindo detalhes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,10 +26,11 @@ const HEROES: Hero[] = [
 
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
